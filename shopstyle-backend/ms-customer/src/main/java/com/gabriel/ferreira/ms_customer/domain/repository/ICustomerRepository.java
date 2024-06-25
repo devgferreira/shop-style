415c1ef10888bbab43184a6bc05f4b11f950fedb
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 17:02:46 -0300

    feat: criando a busca: findByEmail - ICustomerRepository

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/repository/ICustomerRepository.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/repository/ICustomerRepository.java
@@ -4,6 +4,9 @@ import com.gabriel.ferreira.ms_customer.domain.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ICustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
