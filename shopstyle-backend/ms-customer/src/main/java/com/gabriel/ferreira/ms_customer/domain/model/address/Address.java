04fd1782118a40e96b0c203b39a997ea21ab1b8a
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 15:59:38 -0300

    feat: adicioando os atributos - Address

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/Address.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/Address.java
@@ -1,4 +1,24 @@
 package com.gabriel.ferreira.ms_customer.domain.model.address;
 
+import com.gabriel.ferreira.ms_customer.domain.model.customer.Customer;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "address")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String state;
+    private String city;
+    private String street;
+    private Integer number;
+    private String cep;
+    private String complement;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
