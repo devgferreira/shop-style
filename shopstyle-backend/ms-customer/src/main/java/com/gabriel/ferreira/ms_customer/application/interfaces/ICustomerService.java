352211920b0a78b4ef025f665a963a1ab4eddeea
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 16:25:19 -0300

    feat: criando a interface da service: ICustomerService

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.gabriel.ferreira.ms_customer.application.interfaces;
+
+import com.gabriel.ferreira.ms_customer.domain.model.customer.request.CustomerRequest;
+import com.gabriel.ferreira.ms_customer.domain.model.customer.response.CustomerResponse;
+
+public interface ICustomerService {
+
+}
