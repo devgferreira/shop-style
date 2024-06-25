a4c2f568a98c1002e9d550a07ea2aa4c6dadc405
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 15:49:48 -0300

    feat: adicionando os atributos - Customer

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/customer/Customer.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/customer/Customer.java
@@ -1,4 +1,29 @@
 package com.gabriel.ferreira.ms_customer.domain.model.customer;
 
+import com.gabriel.ferreira.ms_customer.domain.enums.Sex;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name="customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String cpf;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Enumerated(value = EnumType.STRING)
+    private Sex sex;
+    private Date birthdate;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private Boolean active;
 }
