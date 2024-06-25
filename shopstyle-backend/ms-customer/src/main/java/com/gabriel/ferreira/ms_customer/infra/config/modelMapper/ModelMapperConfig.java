e2e8ef01d5f2a5eb8f71fe006a8205c31d518905
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 16:31:25 -0300

    feat: configurando o ModelMapper - ModelMapperConfig

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.ms_customer.infra.config.modelMapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
