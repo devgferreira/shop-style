2ba858a0c4c8e2267bee96116ae951524d6a3c91
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 16:28:02 -0300

    feat: Adicioanando a anotação @Repository - IAddressRepository e ICustomerRepository

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/repository/IAddressRepository.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/repository/IAddressRepository.java
@@ -2,6 +2,8 @@ package com.gabriel.ferreira.ms_customer.domain.repository;
 
 import com.gabriel.ferreira.ms_customer.domain.model.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAddressRepository extends JpaRepository<Address, Integer> {
 }
