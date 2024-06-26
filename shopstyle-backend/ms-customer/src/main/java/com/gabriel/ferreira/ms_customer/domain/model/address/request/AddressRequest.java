6c7bed6a3949a964ba9b9fdb994d67a015a209dd
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-26 09:20:44 -0300

    feat: criando o request: AddressRequest

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/request/AddressRequest.java
@@ -0,0 +1,17 @@
+package com.gabriel.ferreira.ms_customer.domain.model.address.request;
+
+import com.gabriel.ferreira.ms_customer.domain.model.customer.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddressRequest {
+    private String state;
+    private String city;
+    private String street;
+    private Integer number;
+    private String cep;
+    private String complement;
+    private Customer customer;
+}
