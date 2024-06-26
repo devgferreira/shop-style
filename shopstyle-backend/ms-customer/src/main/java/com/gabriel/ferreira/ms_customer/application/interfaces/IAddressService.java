61fd4ebfd61abf4e89177b48b96ca1fb34f59814
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-26 09:27:07 -0300

    feat: criando o contrato: criarAddress - IAddressService

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IAddressService.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IAddressService.java
@@ -1,4 +1,8 @@
 package com.gabriel.ferreira.ms_customer.application.interfaces;
 
+import com.gabriel.ferreira.ms_customer.domain.model.address.request.AddressRequest;
+import com.gabriel.ferreira.ms_customer.domain.model.address.response.AddressResponse;
+
 public interface IAddressService {
+    AddressResponse criarAddress(AddressRequest addressRequest);
 }
