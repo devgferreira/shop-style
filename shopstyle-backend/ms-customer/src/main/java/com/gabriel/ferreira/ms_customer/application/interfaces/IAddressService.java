f78476ff422cf2753255392fcb2e512d82c4d804
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-26 10:49:40 -0300

    feat: mudando o retorno do contrato: buscarAddressesPorCustomerId - IAddressService

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IAddressService.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IAddressService.java
@@ -3,9 +3,11 @@ package com.gabriel.ferreira.ms_customer.application.interfaces;
 import com.gabriel.ferreira.ms_customer.domain.model.address.request.AddressRequest;
 import com.gabriel.ferreira.ms_customer.domain.model.address.response.AddressResponse;
 
+import java.util.List;
+
 public interface IAddressService {
     AddressResponse criarAddress(AddressRequest addressRequest);
-    AddressResponse buscarAddressPorCustomerId(Integer customerId);
+    List<AddressResponse> buscarAddressesPorCustomerId(Integer customerId);
     AddressResponse atualizarAddressPorCustomerId(AddressRequest addressRequest, Integer customerId);
     void deleterAddressPorCustomerId(Integer customerId);
 }
