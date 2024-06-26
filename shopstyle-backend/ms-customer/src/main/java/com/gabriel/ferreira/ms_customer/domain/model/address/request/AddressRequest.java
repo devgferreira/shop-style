3b0c533987ae1bf6a47fe816e4ba4b21409a1c9e
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-26 09:39:49 -0300

    feat: adicionando o campo district: AddressRequest

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/request/AddressRequest.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/request/AddressRequest.java
@@ -9,9 +9,10 @@ import lombok.Data;
 public class AddressRequest {
     private String state;
     private String city;
+    private String district;
     private String street;
     private Integer number;
     private String cep;
     private String complement;
-    private String customerId;
+    private Integer customerId;
 }
