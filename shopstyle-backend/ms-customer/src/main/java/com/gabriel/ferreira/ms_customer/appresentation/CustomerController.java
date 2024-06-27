3109f77a5e6ae45be4225850eda4a1a23574aa8f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-27 14:42:01 -0300

    feat: criando o end-point: criarCustomer - CustomerController

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/CustomerController.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/CustomerController.java
@@ -1,9 +1,27 @@
 package com.gabriel.ferreira.ms_customer.appresentation;
 
+import com.gabriel.ferreira.ms_customer.application.interfaces.ICustomerService;
+import com.gabriel.ferreira.ms_customer.domain.model.customer.request.CustomerRequest;
+import com.gabriel.ferreira.ms_customer.domain.model.customer.response.CustomerResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+    private final ICustomerService _customerService;
+
+    public CustomerController(ICustomerService customerService) {
+        _customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> criarCustomer(@RequestBody CustomerRequest customerRequest){
+        CustomerResponse customerResponse = _customerService.criarCustomer(customerRequest);
+        return new ResponseEntity<>(customerResponse, HttpStatus.CREATED);
+    }
 }
