0f00469603ca591e0dd3c14e4cc1d74623742798
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-27 14:45:56 -0300

    feat: criando o end-point: buscarCustomerPorId - CustomerController

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/CustomerController.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/CustomerController.java
@@ -5,10 +5,7 @@ import com.gabriel.ferreira.ms_customer.domain.model.customer.request.CustomerRe
 import com.gabriel.ferreira.ms_customer.domain.model.customer.response.CustomerResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -23,5 +20,10 @@ public class CustomerController {
     public ResponseEntity<CustomerResponse> criarCustomer(@RequestBody CustomerRequest customerRequest){
         CustomerResponse customerResponse = _customerService.criarCustomer(customerRequest);
         return new ResponseEntity<>(customerResponse, HttpStatus.CREATED);
+    }
+    @GetMapping("/id/{customerId}")
+    public ResponseEntity<CustomerResponse> buscarCustomerPorId(@PathVariable Integer customerId){
+        CustomerResponse customerResponse = _customerService.buscarCustomerPorId(customerId);
+        return new ResponseEntity<>(customerResponse, HttpStatus.OK);
     }
 }
