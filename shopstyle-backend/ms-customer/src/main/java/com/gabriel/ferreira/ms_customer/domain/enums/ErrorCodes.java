953807713becf1c5dee9ec84a28b8234f81ff870
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-27 10:50:19 -0300

    feat: criando a enum: CUSTOMER_NAO_ENCONTRADO - ErrorCodes

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/enums/ErrorCodes.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/enums/ErrorCodes.java
@@ -1,4 +1,15 @@
 package com.gabriel.ferreira.ms_customer.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorCodes {
+    CUSTOMER_NAO_ENCONTRADO("Customer não encontrado"),
+
+
+
+
+    private final String mensagem;
 }
