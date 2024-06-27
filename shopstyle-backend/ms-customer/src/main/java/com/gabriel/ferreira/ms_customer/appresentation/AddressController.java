774bd06d8a1262b59efaae96543e2d022c3100fc
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-27 15:00:44 -0300

    feat: criando a controller: AddressController

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/AddressController.java
@@ -0,0 +1,10 @@
+package com.gabriel.ferreira.ms_customer.appresentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AddressController {
+
+}
