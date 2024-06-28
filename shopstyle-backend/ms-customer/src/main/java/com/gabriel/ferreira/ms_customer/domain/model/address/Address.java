c7e5877d4c7be69f0f7309d6850397a102b6c944
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-28 10:53:20 -0300

    feat: criando o response: AddressSemCustomerResponse

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/Address.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/domain/model/address/Address.java
@@ -3,11 +3,15 @@ package com.gabriel.ferreira.ms_customer.domain.model.address;
 import com.gabriel.ferreira.ms_customer.domain.model.customer.Customer;
 import jakarta.persistence.*;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "address")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +26,5 @@ public class Address {
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
 }
