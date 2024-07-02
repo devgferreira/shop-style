7a23fb09af2d9cd2337394427bbae53ccd11f0b9
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-02 11:26:37 -0300

    feat: criando o handler: handleFuncionarioCustomerCpfJaExisteException - RestExceptionHandler

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/exception/handler/RestExceptionHandler.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/exception/handler/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gabriel.ferreira.ms_customer.infra.exception.handler;
 
 import com.gabriel.ferreira.ms_customer.infra.exception.ExceptionResponse;
 import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerCpfInvalidoException;
+import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerCpfJaExisteException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,8 +13,13 @@ import com.gabriel.ferreira.ms_customer.domain.enums.ErrorCodes;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomerCpfInvalidoException.class)
-    public final ResponseEntity<Object> handleFuncionarioNaoEncontradoExeception(CustomerCpfInvalidoException ex) {
+    public final ResponseEntity<Object> handleCustomerCpfInvalidoException(CustomerCpfInvalidoException ex) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.CUSTOMER_CPF_INVALIDO, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
+    }
+    @ExceptionHandler(CustomerCpfJaExisteException.class)
+    public final ResponseEntity<Object> handleFuncionarioCustomerCpfJaExisteException(CustomerCpfJaExisteException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.CUSTOMER_CPF_JA_EXISTE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
     }
 }
