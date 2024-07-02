09e4ab256859dd6bbe5459393abf2046aa445fea
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-02 15:24:19 -0300

    feat: criando a classe de segurança: SecurityConfigurations

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/SecurityConfigurations.java
@@ -0,0 +1,7 @@
+package com.gabriel.ferreira.ms_customer.infra.config.modelMapper.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfigurations {
+}
