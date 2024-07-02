6426252a1d55d5dd12170b53701d4a9311360acf
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-02 11:33:58 -0300

    feat: criando o handler: handleCustomerFirstNameInvalidoException - RestExceptionHandler

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/exception/handler/RestExceptionHandler.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/exception/handler/RestExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.gabriel.ferreira.ms_customer.infra.exception.handler;
 
 import com.gabriel.ferreira.ms_customer.infra.exception.ExceptionResponse;
-import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerCpfInvalidoException;
-import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerCpfJaExisteException;
-import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerEmailInvalidoException;
-import com.gabriel.ferreira.ms_customer.infra.exception.customer.CustomerEmailJaExisteException;
+import com.gabriel.ferreira.ms_customer.infra.exception.customer.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -32,6 +29,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomerEmailJaExisteException.class)
     public final ResponseEntity<Object> handleCustomerEmailJaExisteException(CustomerEmailJaExisteException ex) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.CUSTOMER_EMAIL_JA_EXISTE, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
+    }
+    @ExceptionHandler(CustomerFirstNameInvalidoException.class)
+    public final ResponseEntity<Object> handleCustomerFirstNameInvalidoException(CustomerFirstNameInvalidoException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.CUSTOMER_FIRST_NAME_INVALIDO, ex.getMessage());
         return ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
     }
 }
