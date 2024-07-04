9ef240deae415758ea7d8d193455172c3ee73546
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-04 13:36:17 -0300

    feat: mudando o retorno do método: buscarUserPorEmail - IUserService

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IUserService.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/interfaces/IUserService.java
@@ -1,8 +1,9 @@
 package com.gabriel.ferreira.ms_customer.application.interfaces;
 
 import com.gabriel.ferreira.ms_customer.domain.model.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface IUserService {
     User criarUser(User user);
-    User buscarUserPorEmail(String email);
+    UserDetails buscarUserPorEmail(String email);
 }
