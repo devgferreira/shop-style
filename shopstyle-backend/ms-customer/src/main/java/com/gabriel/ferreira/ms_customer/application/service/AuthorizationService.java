4111221822e5f01d36680d7cef4b0739e39bd2bf
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-04 13:37:11 -0300

    feat: adicionando a anotação @Service - AuthorizationService

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/service/AuthorizationService.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/application/service/AuthorizationService.java
@@ -1,19 +1,22 @@
 package com.gabriel.ferreira.ms_customer.application.service;
 
+import com.gabriel.ferreira.ms_customer.application.interfaces.IUserService;
 import com.gabriel.ferreira.ms_customer.domain.repository.IUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorizationService implements UserDetailsService {
-    private final IUserRepository _userRepository;
+    private final IUserService _userService;
 
-    public AuthorizationService(IUserRepository userRepository) {
-        _userRepository = userRepository;
+    public AuthorizationService(IUserService userService) {
+        _userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return _userRepository.findByEmail(username);
+        return _userService.buscarUserPorEmail(username);
     }
 }
