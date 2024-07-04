4abe407d54205b6116fca0035f73bce269b65972
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-04 11:05:05 -0300

    feat: configuração inicial - SecurityConfigurations

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/SecurityConfigurations.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/SecurityConfigurations.java
@@ -1,7 +1,52 @@
 package com.gabriel.ferreira.ms_customer.infra.config.modelMapper.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests( authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui.html",
+                                "/webjars/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET , "/api/consumers/id/{customerId}")
+                        .hasRole("USER")
+                        .anyRequest().authenticated()
+
+
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"
+        );
+    }
 }
