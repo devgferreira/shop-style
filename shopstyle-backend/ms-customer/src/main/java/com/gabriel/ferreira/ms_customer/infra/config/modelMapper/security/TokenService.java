4b9daaa96ac02330035b1f56b689732341b1f696
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-05 11:49:35 -0300

    build: criando a service: TokenService

---
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/TokenService.java
@@ -0,0 +1,11 @@
+package com.gabriel.ferreira.ms_customer.infra.config.modelMapper.security;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.gabriel.ferreira.ms_customer.domain.model.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+
+}
