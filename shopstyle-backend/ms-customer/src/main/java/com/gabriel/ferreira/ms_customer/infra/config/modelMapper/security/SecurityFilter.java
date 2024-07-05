6579ba4b54cecc39d49498e6d014630603b66c9e
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-05 13:48:47 -0300

    build: implementando o método: recoverToken - SecurityFilter

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/SecurityFilter.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/infra/config/modelMapper/security/SecurityFilter.java
@@ -1,4 +1,36 @@
 package com.gabriel.ferreira.ms_customer.infra.config.modelMapper.security;
 
+import com.gabriel.ferreira.ms_customer.application.interfaces.IUserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
 public class SecurityFilter {
+
+    private  final TokenService _tokenService;
+    private final IUserService _userService;
+
+    public SecurityFilter(TokenService tokenService, IUserService userService) {
+        _tokenService = tokenService;
+        _userService = userService;
+    }
+
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null){
+            return null ;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
 }
