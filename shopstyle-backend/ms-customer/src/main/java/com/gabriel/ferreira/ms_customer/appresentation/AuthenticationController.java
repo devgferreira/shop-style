9f799eb9399bc7b0748d9f7a2110a250edf79742
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-07-05 14:27:42 -0300

    feat: tirando a anotação de segurança - AddressController

--- shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/AuthenticationController.java
+++ shopstyle-backend/ms-customer/src/main/java/com/gabriel/ferreira/ms_customer/appresentation/AuthenticationController.java
@@ -2,18 +2,15 @@ package com.gabriel.ferreira.ms_customer.appresentation;
 
 import com.gabriel.ferreira.ms_customer.domain.model.user.User;
 import com.gabriel.ferreira.ms_customer.domain.model.user.login.Login;
-import com.gabriel.ferreira.ms_customer.infra.config.modelMapper.security.TokenService;
+import com.gabriel.ferreira.ms_customer.infra.config.security.TokenService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/login")
